41a3ef07892a7de396d29309af2d5b76a803c068
Devine <SDevine@doc.gov>
2017-02-03 11:12:31 -0500

    updated file for converting census tiger line shapefiles into dataframe for use in ggplot2 maps

--- shapefile_distress_map_v2.R
+++ shapefile_distress_map_v2.R
@@ -30,32 +30,43 @@ library(readr)
 # might need gpclibPermit() to use fortify if you get error
 # http://stackoverflow.com/questions/30790036/error-istruegpclibpermitstatus-is-not-true
 
-# setwd
-setwd("C:/Users/Stephen/Desktop/R/EDA/shapefile_distress_map/cb_2014_us_cd114_20m")
+#####################################
 
-# read in tiger line file
+
+# load census tiger line shapefiles for states
+
+# setwd to tiger line file
+setwd("C:/Users/Stephen/Desktop/R/EDA/shapefile_distress_map/state_maps/tl_2016_us_state")
+
+# read in tiger line file for 2016 state boundaries
+# https://www.census.gov/geo/maps-data/data/tiger-line.html
+# note that layer = file name for shapefiles
+states_shape <- readOGR(dsn = ".", layer = "tl_2016_us_state")
+names(states_shape)
+gpclibPermit() # may need to install.packages("gpclib") first
+states_shape <- fortify(states_shape, region="GEOID")
+head(states_shape)
+
+setwd("C:/Users/Stephen/Desktop/R/EDA/distress")
+write_csv(states_shape, "states_shapefiles_dataframe.csv")
+
+
+########################################################
+
+
+# load county data
+
+# setwd to tiger line file
+setwd("C:/Users/Stephen/Desktop/R/EDA/shapefile_distress_map/county_maps/tl_2016_us_county")
+
+# read in tiger line file for 2016 counties
+# https://www.census.gov/geo/maps-data/data/tiger-line.html
+# note that layer = file name for shapefiles
 counties_shape <- readOGR(dsn = ".", layer = "tl_2016_us_county")
 names(counties_shape)
-gpclibPermit()
+gpclibPermit() # may need to install.packages("gpclib") first
 counties_shape <- fortify(counties_shape, region="GEOID")
 head(counties_shape)
-
-
-######################################
-
-
-# write hawaii and alaska to file
-
-
-setwd("C:/Users/Stephen/Desktop/R/EDA/shapefile_distress_map")
-
-hawaii <- df %>% filter(state == "HI")
-write_csv(hawaii, "hawaii_shape.csv")
-hawaii <- read_csv("hawaii_shape.csv")
-
-alaska <- df %>% filter(state == "AL")
-write_csv(alaska, "alaska_shape.csv")
-alaska <- read_csv("alaska_shape.csv")
 
 
 #####################################
@@ -63,156 +74,30 @@ alaska <- read_csv("alaska_shape.csv")
 
 # merge data with counties_shape
 setwd("C:/Users/Stephen/Desktop/R/EDA/distress")
-counties <- read_csv("counties_20170131.csv")
+counties <- read_csv("counties_20170201.csv")
 
+# check for overlap
+counties %>% filter(!(counties$fips_state_county %in% counties_shape$id)) %>% select(county_state, fips_state_county)
+
+# merge
 choropleth <- left_join(counties_shape, counties, by = c("id" = "fips_state_county"))
 choropleth <- arrange(choropleth, order)
 
+# inspect counties
+length(unique(counties_shape$id))
+length(unique(choropleth$id))
+length(unique(counties$fips_state_county))
 
-######################################
-
-
-# create alaska map
-choropleth <- alaska
-
-# select color palette using brewer.pal
-# then use colorRampPalette to build a function "pal" which divides the palette by a given number of factors
-# display.brewer.all()
-colors <- brewer.pal(9, "Blues")
-pal <- colorRampPalette(colors)
-# pal is a function, which takes a number as it's argument eg. pal(14)
-# to generalize: pal(length(unique(choropleth$rate_d1)))
-
-# create levels for pc_inc_distress for use in legend names
-choropleth <- choropleth %>% mutate(pc_inc_distress_fct = case_when(.$pc_inc_distress == "0" ~ "Not Distressed", .$pc_inc_distress == "1" ~ "Distressed"))
-choropleth$pc_inc_distress_fct <- factor(choropleth$pc_inc_distress_fct)
-levels <- c("Not Distressed", "Distressed")
-choropleth$pc_inc_distress_fct2 <- factor(choropleth$pc_inc_distress_fct, levels = levels, ordered = TRUE)
+choropleth %>% filter(!(id %in% counties$fips_state_county)) %>% distinct(id) 
 
 
+# filter choropleth shapefiles to just 3142 in counties/pc_inc/bls file
+choropleth <- choropleth %>% filter(id %in% unique(counties$fips_state_county))
 
+# check for overlap
+counties %>% filter(!(unique(counties$fips_state_county) %in% unique(choropleth$id))) %>% 
+        select(county_state, fips_state_county)
+length(unique(choropleth$id))
 
-
-######################################
-
-
-hawaii <- choropleth %>% filter(state == "HI")
-
-p <- ggplot() + geom_polygon(data = hawaii, aes(x = long, y = lat, group = group,
-        fill = unemp_rate), color = "white", size = 0.25) + coord_map() + 
-        scale_fill_distiller(palette = "Blues")
-
-# borrowing my styling from choropleth.examples.R script to make it look cleaner
-# this is the better styling
-p <- ggplot() + geom_polygon(data = df1, aes(x = long, y = lat, group = group,
-                fill = rate), color = "black", size = 0.25) + theme_bw() + 
-                scale_fill_distiller(palette = "Blues", labels = percent, breaks = pretty_breaks(n = 10), values = c(1,0)) +
-                theme(plot.background = element_blank(), panel.grid.major = element_blank(),
-                panel.grid.minor = element_blank(), panel.border = element_blank(), axis.ticks.y = element_blank(), axis.text.y = element_blank(), 
-                axis.ticks.x = element_blank(), axis.text.x = element_blank(), plot.title=element_text(size=20,face="bold")) + 
-                labs(x = "", y = "", title = "Percent Without Health Insurance") + coord_fixed() + coord_map(project = "conic", lat0 = 30)
-
-# without tract boundary lines 
-p <- ggplot() + geom_polygon(data = df1, aes(x = long, y = lat, group = group,
-                                             fill = rate)) + theme_bw() + 
-        scale_fill_distiller(palette = "Blues", labels = percent, breaks = pretty_breaks(n = 10), values = c(1,0)) +
-        theme(plot.background = element_blank(), panel.grid.major = element_blank(),
-              panel.grid.minor = element_blank(), panel.border = element_blank(), axis.ticks.y = element_blank(), axis.text.y = element_blank(), 
-              axis.ticks.x = element_blank(), axis.text.x = element_blank(), plot.title=element_text(size=20,face="bold")) + 
-        labs(x = "", y = "", title = "Percent Without Health Insurance") + coord_fixed() + coord_map(project = "conic", lat0 = 30)
-
-
-ggsave(p, file = "map1.png", width = 6, height = 4.5, type = "cairo-png")
-
-
-
-
-
-# test of reading in census city shapefiles
-# http://www.census.gov/geo/maps-data/data/cbf/cbf_concity.html
-# download shapefile, and copy all files from zipped folder into a working directory
-
-# see census GEOID file for All Places, including Consolidated Cities FIPS
-# http://www.census.gov/geo/maps-data/data/relationship.html
-# variable PLACEFP10 is the CONCIT FIPS code used as the id in the city shapefiles
-
-# general info on consolidated cities
-# http://www.census.gov/geo/reference/webatlas/concities.html
-
-# data seems to only have milford city???
-
-cities <- readOGR(dsn = ".", layer = "cb_2014_09_concity_500k")
-# call structure on cities to find out how to refer to GEOID id variable (eg. "GEO_ID" or "GEOID")
-str(cities)
-cities <- fortify(cities, region="GEOID")
-
-# plot map
-p <- ggplot() + geom_polygon(data = cities, aes(x = long, y = lat, group = group), color = "black", size = 0.25, fill = "red") + coord_map() 
-
-
-
-
-
-# example with noaa cities shapefiles
-# http://www.nws.noaa.gov/geodata/catalog/national/html/cities.htm
-# doesn't work, gpplot can't read spatial data file??
-cities <- readOGR(dsn = ".", layer = "ci08au12")
-# call structure on cities to find out how to refer to GEOID id variable (eg. "GEO_ID" or "GEOID")
-str(cities)
-cities <- fortify(cities, region="ID")
-
-
-
-
-# example with tiger/line cities shapefiles
-# http://www.census.gov/geo/maps-data/data/tiger-line.html
-# only shows milford again, and only has same six state options
-cities <- readOGR(dsn = ".", layer = "tl_2014_09_concity")
-# call structure on cities to find out how to refer to GEOID id variable (eg. "GEO_ID" or "GEOID")
-str(cities)
-cities <- fortify(cities, region="GEOID")
-
-
-
-
-# try with 2012 place file
-# ftp://ftp2.census.gov/geo/tiger/TIGER2012/PLACE/
-
-# this works!
-
-cities <- readOGR(dsn = ".", layer = "tl_2012_01_place")
-# call structure on cities to find out how to refer to GEOID id variable (eg. "GEO_ID" or "GEOID")
-str(cities)
-cities <- fortify(cities, region="GEOID")
-
-state_map <- map_data("state")
-al_map <- filter(state_map, region == "alabama")
-
-p <- ggplot() + geom_polygon(data = cities, aes(x = long, y = lat, group = group), color = "black", size = 0.25, fill = "red") + coord_map() +
-        geom_polygon(data = al_map, aes(x = long, y = lat, group = group) colour = "black", fill = NA)
-
-
-# iterative changes to working ggplot script in choropleth.examples
-ggplot(data = choropleth_input, aes(x = long, y = lat, group = group)) + geom_polygon(data = state_map_input, colour = "black", fill = NA) + 
-        scale_fill_manual(values = pal(length(unique(choropleth_input$rate_d2)))) + theme_bw() + 
-        theme(plot.background = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), 
-        axis.ticks.y = element_blank(), axis.text.y = element_blank(), axis.ticks.x = element_blank(), axis.text.x = element_blank(), 
-        plot.title = element_text(size=20, face = "bold")) + labs(x = "", y = "", title = "Unemployment rate in Alabama", fill = "Unemployment rate") + 
-        coord_fixed() + coord_map(project = "conic", lat0 = 30)
-
-
-ggplot(data = choropleth_input, aes(x = long, y = lat, group = group)) + geom_polygon(data = state_map_input, colour = "black", fill = NA) + theme_bw() + 
-        theme(plot.background = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), 
-              axis.ticks.y = element_blank(), axis.text.y = element_blank(), axis.ticks.x = element_blank(), axis.text.x = element_blank(), 
-              plot.title = element_text(size=20, face = "bold")) + labs(x = "", y = "", title = "Unemployment rate in Alabama", fill = "Unemployment rate") + 
-        coord_fixed() + coord_map(project = "conic", lat0 = 30)
-
-# this works!
-ggplot(data = al_map, aes(x = long, y = lat, group = group)) + 
-        geom_polygon(data = cities, aes(x = long, y = lat, group = group), color = "black", size = 0.25, fill = "red") + 
-        geom_polygon(data = al_map, colour = "black", fill = NA) +theme_bw() + 
-        theme(plot.background = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), 
-              axis.ticks.y = element_blank(), axis.text.y = element_blank(), axis.ticks.x = element_blank(), axis.text.x = element_blank(), 
-              plot.title = element_text(size=20, face = "bold")) + labs(x = "", y = "", title = "Consolidated Cities in Alabama", fill = "Unemployment rate") + 
-        coord_fixed() + coord_map(project = "conic", lat0 = 30)
+write_csv(choropleth, "us_counties_shapefiles_dataframe.csv")
 
